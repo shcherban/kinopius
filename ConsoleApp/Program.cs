f6425b16d5677b04a1eb0033415629f7bc16fe4f
Konstantin Shcherban <kostya.scherban@gmail.com>
2022-11-03 18:01:18 +0300

    add console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter title: ");
+            var title = Console.ReadLine();
+            Console.WriteLine(title);
+            Console.ReadKey();
+        }
+    }
+}
