40543b962c104f560412891002765e124b206e02
Konstantin Shcherban <kostya.scherban@gmail.com>
2022-11-21 23:18:55 +0300

    try OMDb API

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace ConsoleApp
 {
@@ -6,9 +8,26 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter title: ");
-            var title = Console.ReadLine();
-            Console.WriteLine(title);
+            //Console.WriteLine("Enter title: ");
+            //var title = Console.ReadLine();
+
+            string APIKEY = Environment.GetEnvironmentVariable("OMDbApiKey", EnvironmentVariableTarget.User);
+            
+            Console.WriteLine(APIKEY);
+
+            HttpWebRequest request = WebRequest.CreateHttp($"http://www.omdbapi.com/?i=tt3896198&apikey={APIKEY}");
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+            }
+
             Console.ReadKey();
         }
     }
