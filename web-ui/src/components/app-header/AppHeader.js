c2fc0827b591e8907a78c9dc4db527b936f2b8f8
Konstantin Shcherban <kostya.scherban@gmail.com>
2022-12-30 23:30:48 +0300

    add app header

---
+++ web-ui/src/components/app-header/AppHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AppHeader() {
+    return (
+        <div>Шапка</div>
+    )
+}
+
+export default AppHeader;
--- web-ui/src/components/app/App.js
+++ web-ui/src/components/app/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import List from "../list";
+import AppHeader from "../app-header";
 
 function App() {
   return (
     <div className="App">
+      <AppHeader/>
       <List />
     </div>
   );
